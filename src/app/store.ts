ec45a6cfe7e70eb15df1b88b1eb2294332aa5d80
David Johnson <dezekielj@gmail.com>
2026-07-04 00:55:49 -0700

    task-1-e: updates hooks  and exports RootState and Dispatch from store.

--- src/app/store.ts
+++ src/app/store.ts
@@ -6,3 +6,6 @@ export const store = configureStore({
     transactions: transactionsReducer,
   },
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
