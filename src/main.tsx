d50c1d0fff139db682c2a184fe09fcde35da7168
David Johnson <dezekielj@gmail.com>
2026-07-04 01:05:55 -0700

    task-1-f: makes use of useAppSelector and Provider to leverage redux state.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './styles.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles.css";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-)
+);
