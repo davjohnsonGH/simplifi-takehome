0bffff2e06455ab09027da720e442b9f556cd406
David Johnson <dezekielj@gmail.com>
2026-07-04 00:29:31 -0700

    task-1-c: bare bones transactionsSlice, will come back and build out reducers and actions.

---
+++ src/features/transactions/transactionsSlice.ts
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { Transaction } from "../../types";
+import { transactions } from "../../data/transactions";
+
+
+// TODO: fill out reducers, export reducers and actions
+type TransactionsState = {
+    items: Transaction[]
+}
+
+const initialState: TransactionsState = {
+    items: [...transactions]
+}
+const transactionsSlice = createSlice({
+    name: 'transactions',
+    initialState,
+    reducers: {
+        addTransaction: state => state,
+        deleteTransaction: state => state
+    }
+})
