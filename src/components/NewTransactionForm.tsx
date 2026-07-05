86eca6fd857f65119ec5fa22138ebd8ea4f0fc8c
David Johnson <dezekielj@gmail.com>
2026-07-05 15:32:19 -0700

    task-02-h: adds state for form values and onChange implmentation.

--- src/components/NewTransactionForm.tsx
+++ src/components/NewTransactionForm.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 // data for options
 import { TRANSACTION_CATEGORIES } from "../types";
 // components
@@ -6,28 +7,50 @@ import NumberInput from "./NumberInput";
 import TextInput from "./TextInput";
 import Select from "./Select";
 
+export type FormValue = string | number | boolean | FileList | null;
+export type FormValues = Record<string, FormValue>;
+
 interface NewTransactionFormProps {
   //   temp: string;
 }
-const elIdPrefix = "new-transaction-form";
 
 export default function NewTransactionForm({}: NewTransactionFormProps) {
+  const [formValues, setFormValues] = useState<FormValues>({});
+
+  const elIdPrefix = "new-transaction-form";
   const options = TRANSACTION_CATEGORIES.map((category) => ({
     label: category,
     value: category,
   }));
+  function handleChange(fieldId: string, value: FormValue) {
+    setFormValues((previous) => ({
+      ...previous,
+      [fieldId]: value,
+    }));
+  }
+
   return (
     <div>
-      <Datepicker id={elIdPrefix + "-datepicker"} label="Transaction date" />
+      <Datepicker
+        id={elIdPrefix + "-datepicker"}
+        label="Transaction date"
+        onChange={handleChange}
+      />
       <NumberInput
         id={elIdPrefix + "-numberinput"}
         label="Transaction amount"
+        onChange={handleChange}
       />
-      <TextInput id={elIdPrefix + "-textinput"} label="Input merchant" />
+      <TextInput
+        id={elIdPrefix + "-textinput"}
+        label="Input merchant"
+        onChange={handleChange}
+      />
       <Select
         id={elIdPrefix + "-select"}
         label="Select category"
         options={options}
+        onChange={handleChange}
       />
     </div>
   );
