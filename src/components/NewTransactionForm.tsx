666b4fa53317998eed5e4126c56fa751cd2fa6b8
David Johnson <dezekielj@gmail.com>
2026-07-05 14:13:17 -0700

    task-02-e: adds number input component and renders though new transaction form.

--- src/components/NewTransactionForm.tsx
+++ src/components/NewTransactionForm.tsx
@@ -1,14 +1,17 @@
 import Datepicker from "./Datepicker";
+import NumberInput from "./NumberInput";
 interface NewTransactionFormProps {
   //   temp: string;
 }
+const elIdPrefix = "new-transaction-form";
 
 export default function NewTransactionForm({}: NewTransactionFormProps) {
   return (
     <div>
-      <Datepicker
-        id="new-transaction-form-datepicker"
-        label="Transaction date"
+      <Datepicker id={elIdPrefix + "-datepicker"} label="Transaction date" />
+      <NumberInput
+        id={elIdPrefix + "-numberinput"}
+        label="Transaction amount"
       />
     </div>
   );
---
+++ src/components/NumberInput.tsx
@@ -0,0 +1,37 @@
+import type { FormComponentBase } from "./types";
+
+interface NumberInputProps extends FormComponentBase {
+  min?: number;
+  max?: number;
+  step?: string;
+  placeholder?: string;
+}
+
+export default function NumberInput({
+  id,
+  label,
+  required,
+  disabled,
+  min,
+  max,
+  step,
+  placeholder,
+  //   onChange,
+}: NumberInputProps) {
+  return (
+    <>
+      <label htmlFor={id}>{label}</label>
+      <input
+        id={id}
+        type="number"
+        min={min}
+        max={max}
+        step={step}
+        required={required}
+        disabled={disabled}
+        placeholder={placeholder}
+        // onChange={(e) => onChange(id, Number(e.target.value))}
+      />
+    </>
+  );
+}
