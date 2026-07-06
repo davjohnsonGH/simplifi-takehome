c5f3e2870248b7f2990580163b720b9d823d5165
David Johnson <dezekielj@gmail.com>
2026-07-06 14:00:22 -0700

    taks-02: updates types TransactionCategory to also take a string.

--- src/types.ts
+++ src/types.ts
@@ -11,7 +11,9 @@ export const Category = {
   Travel: "Travel",
 } as const;
 
-export type TransactionCategory = (typeof Category)[keyof typeof Category];
+export type TransactionCategory =
+  | (typeof Category)[keyof typeof Category]
+  | string;
 
 export const TRANSACTION_CATEGORIES: readonly TransactionCategory[] =
   Object.values(Category);
@@ -23,6 +25,6 @@ export interface Transaction {
   id: string;
   date: string;
   merchant: string;
-  category: TransactionCategory | string;
+  category: TransactionCategory;
   amount: number;
 }
